ddca9b9bc6a38d2b293b8ae0049385d8f0209920
Danny Lee <dreamer@panix.com>
2019-11-25 19:44:51 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,53 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
+
 class App extends Component {
+
+  state = {
+    pizzas: [],
+    topping: "",
+    size: "",
+    vegetarian: false,
+    selectedOption: ""
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pizzas")
+    .then (resp => resp.json())
+    .then (data => 
+      this.setState({
+        pizzas: data
+      })
+    )
+  }
+
+  editPizza = (pizza) => {
+    let option = pizza.vegetarian ? "Vegetarian" : "Not Vegetarian"
+
+    this.setState({
+      topping: pizza.topping,
+      size: pizza.size,
+      selectedOption: option
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleOptionChange = changeEvent => {
+    this.setState({
+      selectedOption: changeEvent.target.value
+    });
+  };
+  
   render() {
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm handleChange={this.handleChange} handleOptionChange={this.handleOptionChange} state={this.state}/>
+        <PizzaList pizzas={this.state.pizzas} editPizza={this.editPizza}/>
       </Fragment>
     );
   }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,23 @@
 import React from "react"
 
-const PizzaForm = () => {
+const PizzaForm = (props) => {
   return(
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+            <input  type="text" 
+                    className="form-control" 
+                    placeholder="Pizza Topping" 
+                    value={props.state.topping} //Pizza Topping Should Go Here
+                    name="topping"
+                    onChange={props.handleChange}
+            />
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select 
+            name="size"
+            value="{props.state.size}" 
+            onChange={props.handleChange}
+            className="form-control">
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,13 +25,21 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input  className="form-check-input" 
+                    type="radio" 
+                    value="Vegetarian" 
+                    onChange={props.handleOptionChange}
+                    checked={props.state.selectedOption === "Vegetarian"}/>
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input  className="form-check-input" 
+                    type="radio" 
+                    value="Not Vegetarian" 
+                    onChange={props.handleOptionChange}
+                    checked={props.state.selectedOption === "Not Vegetarian"}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
